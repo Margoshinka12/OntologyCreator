95e2aa77d06829de19ffc83a25aa7af443cd0497
margo <margo.kalinina2002@mail.ru>
2023-03-28 17:44:23 +0500

    Добавьте файлы проекта.

---
+++ OntologyCreator/Forms/Metadata.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace OntologyCreator
+{
+    partial class Metadata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Metadata));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOntName = new System.Windows.Forms.TextBox();
+            this.tbOntDescript = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblInterview = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите название онтологии*:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кратко опишите онтологию:";
+            // 
+            // tbOntName
+            // 
+            this.tbOntName.Location = new System.Drawing.Point(235, 7);
+            this.tbOntName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOntName.MaxLength = 50;
+            this.tbOntName.Name = "tbOntName";
+            this.tbOntName.Size = new System.Drawing.Size(308, 22);
+            this.tbOntName.TabIndex = 0;
+            this.tbOntName.TextChanged += new System.EventHandler(this.tbOntoName_TextChanged);
+            // 
+            // tbOntDescript
+            // 
+            this.tbOntDescript.Location = new System.Drawing.Point(235, 39);
+            this.tbOntDescript.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOntDescript.MaxLength = 1000;
+            this.tbOntDescript.Multiline = true;
+            this.tbOntDescript.Name = "tbOntDescript";
+            this.tbOntDescript.Size = new System.Drawing.Size(308, 100);
+            this.tbOntDescript.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblInterview);
+            this.groupBox1.Location = new System.Drawing.Point(19, 196);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(528, 85);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Интервью";
+            // 
+            // lblInterview
+            // 
+            this.lblInterview.Location = new System.Drawing.Point(9, 25);
+            this.lblInterview.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInterview.Name = "lblInterview";
+            this.lblInterview.Size = new System.Drawing.Size(511, 53);
+            this.lblInterview.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = global::OntologyCreator.Properties.Resources.back;
+            this.btnBack.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBack.Location = new System.Drawing.Point(16, 111);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(211, 30);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Назад";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Image = global::OntologyCreator.Properties.Resources.right_arrow;
+            this.btnNext.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNext.Location = new System.Drawing.Point(16, 74);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(211, 30);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Далее";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Модель явлений",
+            "Модель активности",
+            "Модель деятельности",
+            "Модель контекста",
+            "Модель эволюции"});
+            this.comboBox1.Location = new System.Drawing.Point(235, 154);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(308, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 162);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Выберите тип онтологии*:";
+            // 
+            // Metadata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 287);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.tbOntDescript);
+            this.Controls.Add(this.tbOntName);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Metadata";
+            this.Text = "Ввод метаданных";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Metadata_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox tbOntName;
+        private System.Windows.Forms.TextBox tbOntDescript;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblInterview;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
